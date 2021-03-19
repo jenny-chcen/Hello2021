fcadc8b51e889a79e2a7e0600360c4820e40885b
jenny <s1082835@gm.pu.edu.tw>
2021-03-19 13:45:20 +0800

    他人加入螢幕觸控方法

--- app/src/main/java/tw/edu/pu/csim/s1082835/hello2021/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1082835/hello2021/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.csim.s1082835.hello2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var txv: TextView = findViewById<TextView>(R.id.txv)
-        //txv.text = "Hello tcyang"
+        //txv.text = "Hello Jenny~"
+
+        //TestMaster() //呼叫方法
         //TestDEV()
-        //TestMaster()
         txv.setText("嗨，我是陳盈如");
 
     }
@@ -25,6 +27,12 @@ class MainActivity : AppCompatActivity() {
     fun TestDEV(){
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Dev分支"
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "jenny修改螢幕觸控"
+        return true
     }
 
 }
